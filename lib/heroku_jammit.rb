d4cb5167a16612dc37f461527b1af1c99819cb80
Brian Riddle <brian.riddle@tv4.se>
2011-05-27 21:09:10 +0200

    reformatted.

--- lib/heroku_jammit.rb
+++ lib/heroku_jammit.rb
@@ -25,15 +25,15 @@ module Heroku::Command
 
       display "===== Compiling assets...", false
 
-        run "jammit -f"
+      run "jammit -f"
 
       display "===== Commiting assets...", false
 
-        run "git add '#{package_path}' && git commit -m 'assets at #{formatted_date(Time.now)}'"
+      run "git add '#{package_path}' && git commit -m 'assets at #{formatted_date(Time.now)}'"
 
       display "===== Done..."
     end
-    
+
     def delete
       is_root?
 
@@ -42,11 +42,11 @@ module Heroku::Command
       else
         display "===== Deleting compiled assets...", false
 
-          run "rm -rf #{package_path}"
+        run "rm -rf #{package_path}"
 
         display "===== Commiting deleted assets...", false
 
-          run "git rm -rf #{package_path} && git commit -m 'delete assets at #{formatted_date(Time.now)}'"
+        run "git rm -rf #{package_path} && git commit -m 'delete assets at #{formatted_date(Time.now)}'"
 
         display "===== Done..."
       end
@@ -76,47 +76,47 @@ module Heroku::Command
 
     private
 
-      def package_path
-        file = open(config_file_path) {|f| YAML.load(f) }
-        dir = "public/" + (file["package_path"] || "assets")
-      end
+    def package_path
+      file = open(config_file_path) {|f| YAML.load(f) }
+      dir = "public/" + (file["package_path"] || "assets")
+    end
 
-      def config_file_path
-        File.join(Dir.getwd, 'config', 'assets.yml')
-      end
+    def config_file_path
+      File.join(Dir.getwd, 'config', 'assets.yml')
+    end
 
-      def missing_assets?
-        !File.exists? package_path
-      end
+    def missing_assets?
+      !File.exists? package_path
+    end
 
-      def missing_config_file?
-        !File.exists? config_file_path
-      end
+    def missing_config_file?
+      !File.exists? config_file_path
+    end
 
-      def is_root?
-        if missing_config_file?
-          display "app rails not found!, you need stay on the root of one rails app"
-          exit
-        end
+    def is_root?
+      if missing_config_file?
+        display "app rails not found!, you need stay on the root of one rails app"
+        exit
       end
+    end
 
-      def set_branch
-       `git branch`.scan(/^\* (.*)\n/)
-       Regexp.last_match(1)
-      end
+    def set_branch
+      `git branch`.scan(/^\* (.*)\n/)
+      Regexp.last_match(1)
+    end
 
-      def run(cmd)
-        shell cmd
-        if $?.exitstatus == 0
-          display "[OK]"
-        else
-          display "[FAIL]"
-        end
+    def run(cmd)
+      shell cmd
+      if $?.exitstatus == 0
+        display "[OK]"
+      else
+        display "[FAIL]"
       end
+    end
 
-      def formatted_date(date)
-        date.strftime("%A %d, %Y")
-      end
+    def formatted_date(date)
+      date.strftime("%A %d, %Y")
+    end
 
   end
 end
